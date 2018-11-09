9db858476eec03dbe66a8379d7312d566e3775a8
Yi <yy471014@ohio.edu>
2018-11-09 11:10:46 -0500

    insert a new city inside app.js file

--- app.js
+++ app.js
@@ -9,15 +9,38 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
+var citySchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    body: String
+})
+
+var City = mongoose.model('City', citySchema);
+
+var sf = new City({
+    name: "San Francsico",
+    image: "https://pixabay.com/get/eb34b30d2cfd1c22d2524518b7444795ea76e5d004b0144594f7c07ca3e8b3_340.jpg",
+    body: "a city must see"
+})
+
+sf.save();
+
 app.get('/', (req, res) => {
     res.redirect('/travel');
 })
 
 app.get('/travel', (req, res) => {
-    res.render('travel');
+    City.find({}, (err, cities) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('travel', {cities})
+        }
+    })
 })
 
 
 app.listen(3000, function(){
     console.log("SERVER IS RUNNING!");
 })
+
