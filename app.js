6badcfd060f0608e136ca362bbebc3bfbf5c60d9
Yi <yy471014@ohio.edu>
2018-11-09 11:49:06 -0500

    add post route for travel page

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express()
-
+var bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/travel_app', { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 var citySchema = new mongoose.Schema({
@@ -43,7 +44,17 @@ app.get('/travel/new', (req, res) => {
     res.render('new');
 })
 
-
+app.post('/travel', (req, res) => {
+    console.log(req);
+    
+    City.create(req.body.city, (err, city) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('travel');
+        }
+    })
+})
 
 
 app.listen(3000, function(){
