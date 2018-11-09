a406584ce70cddcd4d0f9a2fe47c349a03635fa7
Yi <yy471014@ohio.edu>
2018-11-08 21:46:52 -0500

    set up travel initial page

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express()
+
+
+mongoose.connect('mongodb://localhost/travel_app', { useNewUrlParser: true });
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.redirect('/travel');
+})
+
+app.get('/travel', (req, res) => {
+    res.render('travel');
+})
+
+
+app.listen(3000, function(){
+    console.log("SERVER IS RUNNING!");
+})
